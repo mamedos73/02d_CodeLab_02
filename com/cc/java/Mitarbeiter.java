ae60228a75164589ba8a850ce2c8b06a4216cb01
Roman <roman.makhanbetov@gmx.de>
2022-07-22 17:47:01 +0200

    Mitarbeiter++

---
+++ com/cc/java/Mitarbeiter.java
@@ -0,0 +1,24 @@
+package com.cc.java;
+
+public class Mitarbeiter {
+    
+    private String firstname;
+    private String familyName;
+    private String role;
+    private int yearOfEntry;
+  
+
+    public Mitarbeiter(String firstname, String familyName, String role, int yearOfEntry) {
+        this.firstname = firstname;
+        this.familyName = familyName;
+        this.role = role;
+        this.yearOfEntry = yearOfEntry;
+    }
+
+
+    public String getInfo() {
+        return "";
+    }
+
+}
+
